4522bb6a00d1698b261fbd397c57680ce3e7eebd
ultramenid <malichamdan@gmail.com>
2025-06-25 18:56:41 +0700

    add favicon

--- resources/views/layouts/indexLayout.blade.php
+++ resources/views/layouts/indexLayout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('assets/thumbnail.png') }}" type="image/x-icon">
     <title>{{ $title ?? 'Page Title'}}</title>
     @yield('meta')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
