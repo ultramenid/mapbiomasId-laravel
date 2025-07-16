49830cb581885ac55cad1a58d0529bd64e0bb58b
ultramenid <malichamdan@gmail.com>
2025-07-17 01:17:33 +0700

    newest

--- resources/views/frontends/team-scientific.blade.php
+++ resources/views/frontends/team-scientific.blade.php
@@ -7,11 +7,11 @@
 @section('content')
     @include('partials.topbar')
     <div class="max-w-5xl mx-auto px-4">
-        <h1 class="font-semibold ">KOMITE PENASIHAT AKADEMIK</h1>
+        <h1 class="font-semibold text-center uppercase">{{__('Komite Penasihat Akademik') }}</h1>
         <div class="sm:w-4xl w-full  ">
-            <p class="mt-4 font-light leading-relaxed">Mengingat pentingnya kualitas data dan peta yang dihasilkan, MapBiomas Indonesia memandang perlu keberadaan Komite Penasihat Akademik (Scientific Advisory Committee - SAC) yang berperan memberikan masukan-masukan kepada MapBiomas Indonesia agar senantiasa selaras dengan prinsip dan/atau metode ilmiah.</p>
-            <p class="mt-4 font-light leading-relaxed">Oleh karena itu, pada 2023–sebelum rilis Koleksi 2–MapBiomas Indonesia membentuk Komite Penasihat Akademik yang terdiri atas pakar dan praktisi yang relevan. Keanggotaan komite ini dipilih dengan pertimbangan (i) keterwakilan geografis, (ii) keilmuan yang saling melengkapi, (iii) gender balance, dan (iv) multipihak, dan dengan durasi 2 tahun.</p>
-            <p class="mt-4 font-light leading-relaxed">Anggota Komite Penasihat Akademik MapBiomas Indonesia 2023-2025 adalah sebagai berikut:</p>
+            <p class="mt-4 font-light leading-relaxed">{{__('Mengingat pentingnya kualitas data dan peta yang dihasilkan, MapBiomas Indonesia memandang perlu keberadaan Komite Penasihat Akademik (Scientific Advisory Committee - SAC) yang berperan memberikan masukan-masukan kepada MapBiomas Indonesia agar senantiasa selaras dengan prinsip dan/atau metode ilmiah.') }}</p>
+            <p class="mt-4 font-light leading-relaxed">{{__('Oleh karena itu, pada 2023–sebelum rilis Koleksi 2–MapBiomas Indonesia membentuk Komite Penasihat Akademik yang terdiri atas pakar dan praktisi yang relevan. Keanggotaan komite ini dipilih dengan pertimbangan (i) keterwakilan geografis, (ii) keilmuan yang saling melengkapi, (iii) gender balance, dan (iv) multipihak, dan dengan durasi 2 tahun.') }}</p>
+            <p class="mt-4 font-light leading-relaxed">{{__('Anggota Komite Penasihat Akademik MapBiomas Indonesia 2023-2025 adalah sebagai berikut:') }}</p>
         </div>
 
         {{-- sac --}}
@@ -27,9 +27,9 @@
                 </div>
 
                 <div class="sm:w-6/12 w-full">
-                <h3 class="font-semibold text-sm uppercase">Prof. Projo Danoedoro</h3>
+                <h3 class="font-semibold text-sm ">Prof. PROJO DANOEDORO</h3>
                 <p class="text-gray-700 text-sm leading-relaxed mt-1 font-light">
-                    Guru besar penginderaan jauh Fakultas Geografi Universitas Gadjah Mada ini aktif melakukan riset dan publikasi tulisan akademik mengenai pemetaan tutupan lahan Indonesia. Dapat dikontak melalui surat elektronik projo.danoedoro@geo.ugm.ac.id dan pdanoedoro@ugm.ac.id.
+                    {{__('Guru besar penginderaan jauh Fakultas Geografi Universitas Gadjah Mada ini aktif melakukan riset dan publikasi tulisan akademik mengenai pemetaan tutupan lahan Indonesia. Dapat dikontak melalui surat elektronik projo.danoedoro@geo.ugm.ac.id dan pdanoedoro@ugm.ac.id.') }}
                 </p>
                 </div>
             </div>
@@ -38,14 +38,14 @@
                 <div class="w-52  h-52">
                     <img
                     src="{{ asset('assets/tim/Arif Dermawan 1.png') }}"
-                    alt="Prof. Projo Danoedoro"
+                    alt="Dr. ARIEF DARMAWAN"
                     class="w-full h-full object-cover object-top  rounded-full bg-tim"
                     />
                 </div>
                 <div class="sm:w-6/12 w-full">
-                <h3 class="font-semibold text-sm uppercase">Dr. Arief Darmawan</h3>
+                <h3 class="font-semibold text-sm ">Dr. ARIEF DARMAWAN</h3>
                 <p class="text-gray-700 text-sm leading-relaxed mt-1 font-light">
-                    Dosen Jurusan Kehutanan Fakultas Pertanian Universitas Lampung ini beroleh gelar doktor dari The University of Tokyo. Dapat dikontak melalui surat elektronik arief.darmawan@gmail.com.
+                    {{__('Dosen Jurusan Kehutanan Fakultas Pertanian Universitas Lampung ini beroleh gelar doktor dari The University of Tokyo. Dapat dikontak melalui surat elektronik arief.darmawan@gmail.com.') }}
                 </p>
                 </div>
             </div>
@@ -54,15 +54,15 @@
                 <div class="w-52  h-52">
                     <img
                     src="{{ asset('assets/tim/Dr. Nur Hygiawati Rahayu.png') }}"
-                    alt="Prof. Projo Danoedoro"
+                    alt="Dr. Nur Hygiawati Rahayu"
                     class="w-full h-full object-cover object-top  rounded-full bg-tim"
                     />
                 </div>
 
                 <div class="sm:w-6/12 w-full">
-                <h3 class="font-semibold text-sm uppercase">Dr. Nur Hygiawati Rahayu</h3>
+                <h3 class="font-semibold text-sm ">Dr. NUR HYGIAWATI RAHAYU</h3>
                 <p class="text-gray-700 text-sm leading-relaxed mt-1 font-light">
-                    Pejabat struktural di BAPPENAS/Kementerian Perencanaan Pembangunan Nasional. Memperoleh gelar doktor dari University of Queensland. Dapat dikontak melalui surat elektronik nur.hrahayu@bappenas.go.id.</a>
+                    {{__('Pejabat struktural di BAPPENAS/Kementerian Perencanaan Pembangunan Nasional. Memperoleh gelar doktor dari University of Queensland. Dapat dikontak melalui surat elektronik nur.hrahayu@bappenas.go.id.') }}</a>
                 </p>
                 </div>
             </div>
@@ -70,13 +70,13 @@
             <div class="flex sm:flex-row flex-col sm:items-start  items-center gap-6  mt-12 sm:w-[48%] w-full">
                 <img
                 src="{{ asset('assets/tim/Dr. Hasriani.png') }}"
-                alt="Dr. Hasriani Muis."
+                alt="Dr. HASRIANI MUIS"
                 class="w-52 h-52 object-cover rounded-full bg-tim"
                 />
                 <div>
-                <h3 class="font-semibold text-sm">Dr. Hasriani Muis</h3>
+                <h3 class="font-semibold text-sm">Dr. HASRIANI MUIS</h3>
                 <p class="text-gray-700 text-sm leading-relaxed mt-1 font-light">
-                    Ahli sistem informasi geospasial ini merupakan dosen Fakultas Kehutanan Universitas Tadulako. Dapat dikontak melalui surat elektronik hasrianimuis@gmail.com.</a>.
+                    {{__('Ahli sistem informasi geospasial ini merupakan dosen Fakultas Kehutanan Universitas Tadulako. Dapat dikontak melalui surat elektronik hasrianimuis@gmail.com.') }}</a>.
                 </p>
                 </div>
             </div>
@@ -85,15 +85,15 @@
                 <div class="w-52  h-52">
                     <img
                     src="{{ asset('assets/tim/Francina Frenshegty Kesaulija.png') }}"
-                    alt="Prof. Projo Danoedoro"
+                    alt="Francina Frenshegty Kesaulija"
                     class="w-full h-full object-cover object-top  rounded-full bg-tim"
                     />
                 </div>
 
                 <div class="sm:w-6/12 w-full">
-                <h3 class="font-semibold text-sm uppercase text-nowrap">Francina Frenshegty Kesaulija</h3>
+                <h3 class="font-semibold text-sm  text-nowrap uppercase">Francina Frenshegty Kesaulija</h3>
                 <p class="text-gray-700 text-sm leading-relaxed mt-1 font-light">
-                    Dosen Fakultas Kehutanan Universitas Papua ini memperoleh gelar master dari Australian National University. Dapat dikontak melalui surat elektronik f.kesaulija@unipa.ac.id.</a>.
+                    {{__('Dosen Fakultas Kehutanan Universitas Papua ini memperoleh gelar master dari Australian National University. Dapat dikontak melalui surat elektronik f.kesaulija@unipa.ac.id.') }}</a>.
                 </p>
                 </div>
             </div>
