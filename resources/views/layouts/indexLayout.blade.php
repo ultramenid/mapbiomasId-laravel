127f4be3dbeef87d95392914af293911a85294b7
ultramenid <malichamdan@gmail.com>
2025-07-24 02:39:16 +0700

    forgot the analytic

--- resources/views/layouts/indexLayout.blade.php
+++ resources/views/layouts/indexLayout.blade.php
@@ -15,6 +15,16 @@
     @livewireStyles
 
     @livewireScripts
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-SYKJ4XDS53"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-SYKJ4XDS53');
+    </script>
 </head>
 
 <body class="font-sans">
